d2954a23f2b5f0a20d9a4616fd69613e91827efd
kimyonghyeok <37537394+kimyonghyeok@users.noreply.github.com>
2024-08-29 20:49:13 +0900

    fix

--- Assets/Scripts/BSGScroll.cs
+++ Assets/Scripts/BSGScroll.cs
@@ -5,18 +5,35 @@ using UnityEngine;
 public class BSGScroll : MonoBehaviour
 {
     public MeshRenderer renderer;
+
     public float offset;
     public float speed;
 
-    void Start()
+    public int order;
+    public string layerName;
+    
+    void Awake()
     {
         renderer = GetComponent<MeshRenderer>();
+        renderer.sortingLayerName = layerName;
+        renderer.sortingOrder = order;
     }
 
     void Update()
     {
+        if (renderer.sortingLayerName != layerName)
+            renderer.sortingLayerName = layerName;
+        if (renderer.sortingOrder != order)
+            renderer.sortingOrder = order;
+
         offset += Time.deltaTime * speed;
         renderer.material.mainTextureOffset = new Vector2(offset, 0);
     }
 
+    public void OnValidate()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        renderer.sortingLayerName = layerName;
+        renderer.sortingOrder = order;
+    }
 }
