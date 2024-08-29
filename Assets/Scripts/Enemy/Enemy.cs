7cef9c2c5eef5f2ed684510ec635d5adee221956
Superdodge <yjh01209@gmail.com>
2024-08-29 21:27:25 +0900

    Update Spawn logic

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,11 +6,8 @@ using DG.Tweening;
 public class Enemy : MonoBehaviour
 {
     [SerializeField]
-    private Sprite _spriteRed;
-    [SerializeField]
-    private Sprite _spriteBlue;
-    [SerializeField]
     private SpriteRenderer _spriteRenderer;
+    public Sprite sprite;
     public EnemyTypes enemyType;
     public float spawnTime;
     public float spawnSpeed;
@@ -18,8 +15,7 @@ public class Enemy : MonoBehaviour
 
     void Start(){
 
-        if(enemyType == EnemyTypes.RED) _spriteRenderer.sprite = _spriteRed;
-        else if(enemyType == EnemyTypes.BLUE) _spriteRenderer.sprite = _spriteBlue;
+        _spriteRenderer.sprite = sprite;
 
         transform.DOMoveX(CharX, spawnSpeed).SetEase(Ease.Linear);
     }
