5b84f82ecdb716577cb19c4321c5e92462b02820
bacsal <50982614+bacsal@users.noreply.github.com>
2024-08-30 00:07:16 +0900

    Sliding

--- Assets/0_Lee/CharacterMovement.cs
+++ Assets/0_Lee/CharacterMovement.cs
@@ -9,13 +9,19 @@ using Unity.VisualScripting;
 public class CharacterMovement : MonoBehaviour
 {
     private bool isJumping = false;
+    private bool isSliding = false;
 
     public float jumpPower = 20;
+
+    public Collider2D Running;
+    public Collider2D Sliding;
 
     private Rigidbody2D rigid;
     // Start is called before the first frame update
     void Start()
     {
+        Running.enabled = true;
+        Sliding.enabled = false;
         rigid = GetComponent<Rigidbody2D>();
     }
     
@@ -23,10 +29,26 @@ public class CharacterMovement : MonoBehaviour
     {
         //Jump
         if (Input.GetKeyDown(KeyCode.Q) && !isJumping)
-        {
+        {   Running.enabled = true;
+            Sliding.enabled = false;
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             //anim.SetBool("isjumping", true);
             isJumping = true;
+        }
+
+        if (Input.GetKey(KeyCode.W) &&!isJumping && !isSliding)
+        {   
+            Debug.Log("Sliding");
+            Running.enabled = false;
+            Sliding.enabled = true;
+            isSliding = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) && isSliding)
+        {
+            Running.enabled = true;
+            Sliding.enabled = false;
+            isSliding = false;
         }
     }
     void FixedUpdate()
@@ -36,12 +58,13 @@ public class CharacterMovement : MonoBehaviour
         {
             Debug.Log("falling");
             Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
-            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 15, LayerMask.GetMask("Platform"));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 50, LayerMask.GetMask("Platform"));
             Debug.Log(rayHit.collider);
             if (rayHit.collider != null)
             {
-                if (rayHit.distance < 1.5)
+                if (rayHit.distance < 2)
                 {
+                    Debug.Log("Floor");
                     isJumping = false;
                     //anim.SetBool("isjumping", false);
                 }
