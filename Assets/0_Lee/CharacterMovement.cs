588f873cdae553c92da89f5ee971db44391cc885
bacsal <50982614+bacsal@users.noreply.github.com>
2024-08-29 22:06:27 +0900

    d

--- Assets/0_Lee/CharacterMovement.cs
+++ Assets/0_Lee/CharacterMovement.cs
@@ -10,16 +10,18 @@ public class CharacterMovement : MonoBehaviour
 {
     private bool isJumping = false;
 
-    private Rigidbody2D rid;
+    public float jumpPower = 20;
+
+    private Rigidbody2D rigid;
     // Start is called before the first frame update
     void Start()
     {
-        rid = GetComponent<Rigidbody2D>();
+        rigid = GetComponent<Rigidbody2D>();
     }
-
+/*
     void OnJumpStart()
     {
-        rid.AddForce(new Vector2(0,13),ForceMode2D.Impulse);
+        rigid.AddForce(new Vector2(0,13),ForceMode2D.Impulse);
         
     }
 
@@ -39,5 +41,32 @@ public class CharacterMovement : MonoBehaviour
             OnJumpStart();
         }
         Debug.Log(isJumping);
+    }*/
+    
+    
+    void Update()
+    {
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            //anim.SetBool("isjumping", true);
+        }
+    }
+    void FixedUpdate()
+    {
+        //Landing Ploatform
+        if(rigid.velocity.y < 0) //내려갈떄만 스캔
+        {
+            Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                {
+                    //anim.SetBool("isjumping", false);
+                }
+            }
+        }
     }
 }
